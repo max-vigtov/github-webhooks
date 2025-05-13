a36857f6eb5b409edbd2c899c959f7de39c6d9e4
Max Vigueras <mvt.2000@hotmail.com>
2025-05-13 01:08:05 -0600

    Webhook: Security with secret key & middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
 import { GithubService, DiscordService } from './presentation/services';
+import { GithubSha256Middleware } from './presentation/middlewares';
 
 (() => {
 	main();
@@ -17,6 +18,8 @@ function main() {
 	const controller = new GithubController( githubService, discordService );
 
 	app.use( express.json() );
+	app.use( GithubSha256Middleware.verifySignature );
+
 	app.post('/api/github', controller.webhookHandler);
 
 	app.listen( envs.PORT, () => {
